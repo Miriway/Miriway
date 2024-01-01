58fac84a9c3a049d2e71ffc125e157a906897aa8
Alan Griffiths <alan@octopull.co.uk>
2024-01-01 17:53:06 +0000

    Extract workspace management (#80)
    * First cut at extracting WorkspaceManager
    
    * Take implementations out of header
    
    * Better encapsulation
    
    * Extract WorkspaceManager integration logic into a helper class

--- miriway_policy.h
+++ miriway_policy.h
@@ -19,10 +19,9 @@
 #ifndef MIRIWAY_POLICY_H_
 #define MIRIWAY_POLICY_H_
 
-#include <miral/minimal_window_manager.h>
+#include "miriway_workspace_manager.h"
 
-#include <map>
-#include <list>
+#include <miral/minimal_window_manager.h>
 
 namespace miriway
 {
@@ -31,19 +30,18 @@ class ShellCommands;
 
 // A window management policy that adds support for docking and workspaces.
 // Co-ordinates with `ShellCommands` for the handling of related commands.
-class WindowManagerPolicy :
-    public MinimalWindowManager
+class WindowManagerPolicy : public WorkspaceWMStrategy<MinimalWindowManager>
 {
 public:
     WindowManagerPolicy(WindowManagerTools const& tools, ShellCommands& commands);
 
+    using WorkspaceWMStrategy::workspace_begin;
+    using WorkspaceWMStrategy::workspace_end;
+    using WorkspaceWMStrategy::workspace_up;
+    using WorkspaceWMStrategy::workspace_down;
     void dock_active_window_left();
     void dock_active_window_right();
     void toggle_maximized_restored();
-    void workspace_begin(bool take_active);
-    void workspace_up(bool take_active);
-    void workspace_down(bool take_active);
-    void workspace_end(bool take_active);
 
 private:
     auto place_new_window(ApplicationInfo const& app_info, WindowSpecification const& request_parameters)
@@ -53,28 +51,11 @@ private:
 
     void advise_delete_window(const WindowInfo &window_info) override;
 
-    void handle_modify_window(WindowInfo& window_info, WindowSpecification const& modifications) override;
-
-    void advise_adding_to_workspace(std::shared_ptr<Workspace> const& workspace,
-                                    std::vector<Window> const& windows) override;
-
     bool handle_keyboard_event(MirKeyboardEvent const* event) override;
 
-    void apply_workspace_hidden_to(Window const& window);
-    void apply_workspace_visible_to(Window const& window);
-    void change_active_workspace(std::shared_ptr<Workspace> const& ww,
-                                 std::shared_ptr<Workspace> const& old_active,
-                                 miral::Window const& window);
-    using workspace_list = std::list<std::shared_ptr<Workspace>>;
-    void erase_if_empty(workspace_list::iterator const& old_workspace);
     void dock_active_window_under_lock(MirPlacementGravity placement);
 
     ShellCommands* const commands;
-
-    workspace_list workspaces;
-    workspace_list::iterator active_workspace;
-
-    std::map<std::shared_ptr<miral::Workspace>, miral::Window> workspace_to_active;
 };
 }
 
---
+++ miriway_workspace_manager.h
@@ -0,0 +1,133 @@
+/*
+ * Copyright © 2023 Octopull Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Alan Griffiths <alan@octopull.co.uk>
+ */
+
+#ifndef MIRIWAY_WORKSPACE_MANAGER_H_
+#define MIRIWAY_WORKSPACE_MANAGER_H_
+
+#include <miral/window_manager_tools.h>
+
+#include <list>
+#include <map>
+
+namespace miriway
+{
+using miral::Window;
+using miral::WindowInfo;
+using miral::WindowManagerTools;
+using miral::WindowSpecification;
+using miral::Workspace;
+
+class WorkspaceManager
+{
+public:
+    explicit WorkspaceManager(WindowManagerTools const& tools);
+    virtual ~WorkspaceManager() = default;
+
+    void workspace_begin(bool take_active);
+
+    void workspace_end(bool take_active);
+
+    void workspace_up(bool take_active);
+
+    void workspace_down(bool take_active);
+
+    void apply_workspace_hidden_to(Window const& window);
+
+    void apply_workspace_visible_to(Window const& window);
+
+    void change_active_workspace(std::shared_ptr<Workspace> const& ww,
+                                 std::shared_ptr<Workspace> const& old_active,
+                                 miral::Window const& window);
+
+
+    void advise_new_window(const WindowInfo &window_info);
+
+    void advise_adding_to_workspace(std::shared_ptr<Workspace> const& workspace,
+                                    std::vector<Window> const& windows);
+
+    auto active_workspace() const -> std::shared_ptr<Workspace>;
+
+    bool in_hidden_workspace(WindowInfo const& info) const;
+
+    static bool is_application(MirDepthLayer layer);
+
+    struct WorkspaceInfo;
+
+    static auto make_workspace_info() -> std::shared_ptr<WorkspaceInfo>;
+
+    // This implementation assumes the userdata contains WorkspaceInfo
+    // but, in case there's another option, made virtual
+    virtual auto workspace_info_for(WindowInfo const& info) const ->  WorkspaceInfo&
+    {
+        return *std::static_pointer_cast<WorkspaceInfo>(info.userdata());
+    }
+
+private:
+    WindowManagerTools tools_;
+
+    using workspace_list = std::list<std::shared_ptr<Workspace>>;
+
+    workspace_list workspaces;
+    workspace_list::iterator active_workspace_;
+    std::map<std::shared_ptr<miral::Workspace>, miral::Window> workspace_to_active;
+
+    void erase_if_empty(workspace_list::iterator const& old_workspace);
+};
+
+// Template class to hook WorkspaceManager into a window management strategy
+template<typename WMStrategy>
+class WorkspaceWMStrategy : public WMStrategy, protected WorkspaceManager
+{
+protected:
+    explicit WorkspaceWMStrategy(WindowManagerTools const& tools) :
+        WMStrategy{tools},
+        WorkspaceManager{tools}
+    {}
+
+    void advise_new_window(const WindowInfo &window_info) override
+    {
+        WMStrategy::advise_new_window(window_info);
+        WorkspaceManager::advise_new_window(window_info);
+    }
+
+    void advise_adding_to_workspace(std::shared_ptr<Workspace> const& workspace,
+                                    std::vector<Window> const& windows) override
+    {
+        WMStrategy::advise_adding_to_workspace(workspace, windows);
+        WorkspaceManager::advise_adding_to_workspace(workspace, windows);
+    }
+
+    void handle_modify_window(WindowInfo& window_info, WindowSpecification const& modifications) override
+    {
+        auto mods = modifications;
+
+        if (in_hidden_workspace(window_info))
+        {
+            // Don't allow state changes in hidden workspaces
+            if (mods.state().is_set()) mods.state().consume();
+
+            // Don't allow size changes in hidden workspaces
+            if (mods.size().is_set()) mods.size().consume();
+        }
+
+        WMStrategy::handle_modify_window(window_info, mods);
+    }
+};
+} // miriway
+
+#endif //MIRIWAY_WORKSPACE_MANAGER_H_
