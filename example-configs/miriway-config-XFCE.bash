ef6055294002f0504789662db86b9884705724cc
Alan Griffiths <alan@octopull.co.uk>
2024-10-17 10:56:37 +0100

    Export the whole family of XDG environment variables to systemd

--- example-configs/miriway-config-XFCE.bash
+++ example-configs/miriway-config-XFCE.bash
@@ -61,7 +61,7 @@ cat <<EOT > "${miriway_config}"
 x11-window-title=Miriway
 idle-timeout=600
 app-env-amend=XDG_SESSION_TYPE=wayland:GTK_USE_PORTAL=0:XDG_CURRENT_DESKTOP=Miriway:GTK_A11Y=none
-shell-component=dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY XDG_SESSION_TYPE XDG_CURRENT_DESKTOP
+shell-component=miriway-unsnap dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY XDG_DATA_HOME XDG_CONFIG_HOME XDG_CONFIG_DIRS XDG_DATA_DIRS XDG_CACHE_HOME XDG_DESKTOP_DIR XDG_SESSION_TYPE XDG_SESSION_DESKTOP XDG_CURRENT_DESKTOP
 
 shell-component=miriway-unsnap swaybg --mode fill --output '*' --image '${background}'
 shell-component=miriway-unsnap xfce4-panel
