173febbd16d9e570b3f22e9de0627b17542afb1d
Alan Griffiths <alan@octopull.co.uk>
2025-04-03 10:34:47 +0100

    Skeleton of implementation

---
+++ miriway_ext_workspace_v1.cpp
@@ -0,0 +1,62 @@
+/*
+* Copyright © 2025 Octopull Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Alan Griffiths <alan@octopull.co.uk>
+ */
+
+#include "miriway_ext_workspace_v1.h"
+
+void miriway::ExtWorkspaceManagerV1::commit()
+{
+}
+
+void miriway::ExtWorkspaceManagerV1::stop()
+{
+}
+
+void miriway::ExtWorkspaceManagerV1::Global::bind(wl_resource* new_ext_workspace_manager_v1)
+{
+    new ExtWorkspaceManagerV1{new_ext_workspace_manager_v1, mir::wayland::Resource::Version<1>{}};
+}
+
+void miriway::ExtWorkspaceGroupHandleV1::create_workspace(std::string const& workspace)
+{
+    (void)workspace;
+}
+
+void miriway::ExtWorkspaceGroupHandleV1::destroy()
+{
+}
+
+void miriway::ExtWorkspaceHandleV1::destroy()
+{
+}
+
+void miriway::ExtWorkspaceHandleV1::activate()
+{
+}
+
+void miriway::ExtWorkspaceHandleV1::deactivate()
+{
+}
+
+void miriway::ExtWorkspaceHandleV1::assign(wl_resource* workspace_group)
+{
+    (void)workspace_group;
+}
+
+void miriway::ExtWorkspaceHandleV1::remove()
+{
+}
---
+++ miriway_ext_workspace_v1.h
@@ -0,0 +1,59 @@
+/*
+ * Copyright © 2025 Octopull Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Alan Griffiths <alan@octopull.co.uk>
+ */
+
+#ifndef MIRIWAY_EXT_WORKSPACE_V1_H
+#define MIRIWAY_EXT_WORKSPACE_V1_H
+
+#include "wayland-generated/ext-workspace-v1_wrapper.h"
+
+namespace miriway
+{
+class ExtWorkspaceManagerV1 : public mir::wayland::ExtWorkspaceManagerV1
+{
+public:
+    using mir::wayland::ExtWorkspaceManagerV1::ExtWorkspaceManagerV1;
+    void commit() override;
+    void stop() override;
+
+    class Global;
+};
+
+class ExtWorkspaceManagerV1::Global : public mir::wayland::ExtWorkspaceManagerV1::Global
+{
+public:
+    void bind(wl_resource* new_ext_workspace_manager_v1) override;
+};
+
+class ExtWorkspaceGroupHandleV1 :  public mir::wayland::ExtWorkspaceGroupHandleV1
+{
+    void create_workspace(std::string const& workspace) override;
+    void destroy() override;
+};
+
+class ExtWorkspaceHandleV1 : public mir::wayland::ExtWorkspaceHandleV1
+{
+public:
+    void destroy() override;
+    void activate() override;
+    void deactivate() override;
+    void assign(wl_resource* workspace_group) override;
+    void remove() override;
+};
+} // miriway
+
+#endif //MIRIWAY_EXT_WORKSPACE_V1_H
